ea60a704e76d849a574b4f15beaab111ec383e98
Matt Herbst <m.herbst@my.ccsu.edu>
2025-12-04 02:41:52 -0500

    change login button from a to link

--- src/WebPages/SiteComps/LoginButton.jsx
+++ src/WebPages/SiteComps/LoginButton.jsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 import { Siteroutes } from "./ScriptFuntions/Siteroutes";
 
 export default function LoginButton(){
     return(
 <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse h-16">
-<a href={Siteroutes.LoginSignup} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 font-bold">Login / SignUp</a>
+<Link to={Siteroutes.LoginSignup} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-700 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 font-bold">Login / SignUp</Link>
 </div>
     );
 }
