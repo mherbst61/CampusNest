d2c15860afd8130a62682bbc6952cfd80e5d459e
Matt Herbst <m.herbst@my.ccsu.edu>
2025-12-04 02:46:06 -0500

    change dashboard buttons to links

--- src/WebPages/SiteComps/NavBarComps/UserProfile.jsx
+++ src/WebPages/SiteComps/NavBarComps/UserProfile.jsx
@@ -42,10 +42,10 @@ const UserProfileImg = user.photoURL || "src\assets\question-mark.png";
 
     <ul className="py-2" aria-labelledby="user-menu-button">
       <li>
-        <a href={Siteroutes.DashBoard} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+        <Link to={Siteroutes.DashBoard} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</Link>
       </li>
       <li>
-        <a href={Siteroutes.Profile} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profile</a>
+        <Link to={Siteroutes.Profile} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profile</Link>
       </li>
   <li>
        {/*THIS IS THE SIGN OUT BUTTON*/} <a type="Button" onClick={async () => { await signOutUser(); setOpen(false); }} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
