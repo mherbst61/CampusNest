2392550567221be19d470404c02818920d71b1b8
Matt Herbst <m.herbst@my.ccsu.edu>
2025-12-04 03:57:37 -0500

    remove reinitalizer

--- src/WebPages/SiteComps/navBar.jsx
+++ src/WebPages/SiteComps/navBar.jsx
@@ -7,10 +7,6 @@ export default function Navbar() {
 
   {/*This sets the variable state for logged in to change button*/}
 const [isLoggedIn, setIsLoggedIn] = useState(false);
-   useEffect(() => {
-    // Re-import or re-initialize Flowbite if needed
-    import('flowbite');
-  }, []);
 
   return (
  <nav className="w-full bg-white border-gray-200 dark:bg-blue-500">
