d6b985cb069c7bf74e8551b9173167a40444ce8f
Matt Herbst <m.herbst@my.ccsu.edu>
2025-12-04 19:42:30 -0500

    trying a internet solution doutful it will work

--- src/WebPages/SiteComps/navBar.jsx
+++ src/WebPages/SiteComps/navBar.jsx
@@ -31,11 +31,10 @@ useEffect(()=>{
   </a>
   
   {/*Login button state and dropdown when shrunk for nav menu*/}
-  <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse relative w-full md:w-auto">
-    <div className="justify-end">
+  <div className="flex items-center w-full md:w-auto md:order-2 
+                justify-between md:justify-end space-x-3 md:space-x-0 relative">
       <LoginButtonState />
-    </div>
-       <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-end text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
+       <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
