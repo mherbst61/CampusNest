c17792e288476f80504bf8d11f03c5bb0192e421
Matt Herbst <m.herbst@my.ccsu.edu>
2025-12-04 19:38:16 -0500

    trying again to adjust button and dropdown nav menu

--- src/WebPages/SiteComps/navBar.jsx
+++ src/WebPages/SiteComps/navBar.jsx
@@ -31,9 +31,9 @@ useEffect(()=>{
   </a>
   
   {/*Login button state and dropdown when shrunk for nav menu*/}
-  <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse relative w-full md:w-auto justify-end">
-    <div className="justify-start">
- <LoginButtonState />
+  <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse relative w-full md:w-auto">
+    <div className="justify-end">
+      <LoginButtonState />
     </div>
        <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-end text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
